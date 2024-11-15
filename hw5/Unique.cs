1357004cd104c26ae46961a539453a17568287cd
alterkyrie <32509647+alterkyrie@users.noreply.github.com>
2024-11-15 23:20:58 +0000

    feat: update to bruteforce

--- hw5/Unique.cs
+++ hw5/Unique.cs
@@ -1,15 +1,26 @@
 public static class Unique
 {
+    private static Random _random = new Random();
     public static void Run()
     {
-        var input = "bbbbb";
+        var input = GetRandomInpit(5 * 10000);
         var result = BruteForce(input);
-        Console.WriteLine(result);
+        Console.WriteLine($"{input} - {result}");
+    }
+    private static string GetRandomInpit(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$>%^&*()";
+        const string chars_short = "ABC";
+
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[_random.Next(s.Length)]).ToArray());
     }
     private static int BruteForce(string s)
     {
+        if (string.IsNullOrEmpty(s))
+            return 0;
         var buffer = "";
-        var count = 0;
+        var count = 1;
         for (int i = 0; i < s.Length; i++)
         {
             var c = s[i];
@@ -24,5 +35,9 @@ public static class Unique
             }
         }
         return count;
+    }
+    private static int TwoPointers(string s)
+    {
+        
     }
 }
--- template/HackerrankTemplate.cs
+++ template/HackerrankTemplate.cs
@@ -6,10 +6,32 @@ public class Solution
     {
         var reader = new StreamReader(Console.OpenStandardInput());
         var writer = new StreamWriter(Console.OpenStandardOutput());
-        var a = int.Parse(reader.ReadLine());
-        var b = int.Parse(reader.ReadLine());
-        writer.WriteLine(a + b);
+        var s = reader.ReadLine();
+        var result = BruteForce(s);
+        writer.WriteLine(result);
         reader.Close();
         writer.Close();
     }
+    private static long BruteForce(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return 0;
+        var buffer = "";
+        var count = 1L;
+        for (var i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+            if (buffer.Contains(c))
+            {
+                count = Math.Max(count, buffer.Length);
+                buffer = $"{c}";
+            }
+            else
+            {
+                buffer += c;
+            }
+        }
+        return count;
+    }
+
 }
